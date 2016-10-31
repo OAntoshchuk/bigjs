02036d25c4cf0bf6f86dc681225936aa935a4ebd
OAntoshchuk <zzalex.km@gmail.com>
2016-10-31 20:00:23 +0200

    my project

---
+++ public/list.js
@@ -0,0 +1,107 @@
+const list = [
+	    {
+	        id : 1,
+	        name : 'Audi R8',
+	        description : 'Audi R8 V10 EU-spec ',
+	        date : '16 июня 2016 г',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/audi/r8/autowp.ru_audi_r8_v10_4.jpeg'
+	    },
+	    {
+        id : 2,
+	        name : 'BMW M5',
+	        description : 'BMW M5 Touring Worldwide (E61) 03.2007–04.2010 ',
+	        date : '19 октября 2016 г.',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/b/bmw/m5_touring/bmw_m5_touring_18.jpeg'
+	    },
+	    {
+	        id : 3,
+	        name : 'Cadillac Escalade',
+	        description : 'Cadillac Escalade 1999–2000',
+	        date : '25 мая 2016 г.',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/cadillac/escalade/autowp.ru_cadillac_escalade_44.jpeg'
+	    },
+	    {
+	        id : 4,
+	        name : 'Dodge Viper',
+	        description : 'Dodge Viper SRT10 Coupe 2006–07',
+	        date : '11 февраля 2016 г.',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/d/dodge/srt/viper_10_coupe/dodge_viper_srt10_coupe_13.jpeg'
+	    },
+	    {
+	        id : 5,
+	        name : 'Ford Mustang',
+	        description : 'Shelby GT350R Mustang',
+	        date : '5 августа 2016 г.',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/autowp_1722345448.jpeg'
+	    },
+	    {
+	        id : 6,
+	        name : 'GMC Acadia Denali',
+	        description : 'GMC Acadia Denali',
+	        date : '27 июля 2016 г.',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery/g/gmc/acadia_denali/gmc_acadia_denali_34.jpg'
+	    },
+	    {
+	        id : 7,
+	        name : 'Honda Civic Type-R',
+	        description : 'Honda Civic Type-R (EK9) 08.1997–09.2001',
+	        date : '3 августа 2016 г.',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/honda/civic/autowp.ru_honda_civic_type-r_108.jpeg'
+	    },
+	    {
+	        id : 8,
+	        name : 'Infiniti QX56',
+	        description : 'Infiniti QX56 (JA60) 2004–07',
+	        date : '29 сентября 2016 г.',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/infiniti/qx_series/autowp.ru_infiniti_qx56_39.jpeg'
+	    },
+	    {
+	        id : 9,
+	        name : 'Jaguar XF',
+	        description : 'Jaguar XF 25t R-Sport AU-spec 2016',
+	        date : '21 июля 2016 г.',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery/j/jaguar/xf_r-sport/jaguar_xf_r-sport_44.jpg'
+	    },
+	    {
+	        id : 10,
+	        name : 'Kawasaki Ninja',
+	        description : 'Kawasaki Ninja ZX-10R North America 2016',
+	        date : '13 октября 2016 г.',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/k/kawasaki/zx-10r/kawasaki_zx-10r_4.jpg'
+	    },    
+	    {
+	        id : 11,
+	        name : 'Lamborghini Gallardo',
+	        description : 'Lamborghini Gallardo LP 560-4 2008–12',
+	        date : 'July 15th, 2016',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/l/lamborghini/gallardo_lp_560-4/lamborghini_gallardo_lp_560-4_8.jpg'
+	    },    
+	    {
+	        id : 12,
+	        name : 'Mazda 626',
+	        description : 'Mazda 626 Sedan (CB) 10.1978–80',
+	        date : 'July 8th, 2016',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/mazda/626/autowp.ru_mazda_626_sedan_17.jpeg'
+	    },    
+	    {
+	        id : 13,
+	        name : 'Nissan Fuga',
+	        description : 'Nissan Fuga Hybrid (Y51) 01.2015–н.в.',
+	        date : 'June 24th, 2016',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery/n/nissan/fuga/nissan_fuga_9.jpeg'
+	    },    
+	    {
+	        id : 14,
+	        name : 'Opel GT',
+	        description : 'Opel GT 2006–07.2009',
+	        date : 'April 15th, 2016',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/opel/gt/autowp.ru_opel_gt_4.jpeg'
+	    },    
+	    {
+	        id : 15,
+	        name : 'Porsche Panamera',
+	        description : 'Porsche Panamera GTS Worldwide (970) 02.2012–13',
+	        date : 'May 27th, 2016',
+	        href : 'http://i.wheelsage.org/image/format/picture/picture-gallery-full/porsche/panamera/autowp.ru_porsche_panamera_gts_34.jpeg'
+	    }  
+	];
